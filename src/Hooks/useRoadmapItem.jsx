e9089a3d0f4d88c66ec25fd94f3489e0c26eeee8
Md Shorif Uddin <mdshorifuddin463@gmail.com>
2025-06-22 23:13:11 +0600

    implement sort and filter system

--- src/Hooks/useRoadmapItem.jsx
+++ src/Hooks/useRoadmapItem.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import useAxiosSecure from './useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 
-const useRoadmapItem = () => {
+const useRoadmapItem = (filter = 'allItem', sort = '') => {
       const axiosSecure = useAxiosSecure();
 
       const {data :roadmapItem = [],isLoading,refetch} = useQuery({
-        queryKey : ['roadmap-item'],
+        queryKey : ['roadmap-item',filter,sort],
         queryFn : async () => {
-            const res = await axiosSecure.get('roadmap-item');
+            const res = await axiosSecure.get(`/roadmap-item?filter=${filter}&sort=${sort}`);
             return res.data
         }
       })
--- src/Pages/Road-map-item/RoadMapItem.jsx
+++ src/Pages/Road-map-item/RoadMapItem.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useRoadmapItem from '../../Hooks/useRoadmapItem';
 import RoadMapCard from './RoadMapCard';
 import Loading from '../../Components/Loading';
 
 
 const RoadMapItem = () => {
-    const [roadmapItem,isLoading,refetch] = useRoadmapItem();
+     const [filter,setFilter] = useState('allItem');
+     const [sort,setSort] = useState('');
+    const [roadmapItem,isLoading,refetch] = useRoadmapItem(filter,sort);
 
     
     if(isLoading){
        return <Loading></Loading>
+    }
+
+    const handleResetBtn = () => {
+       setFilter('allItem');
+       setSort('')
     }
 
     
@@ -19,22 +26,21 @@ const RoadMapItem = () => {
           <div className='w-full mb-3 roadmap_action_controls bg-white shadow-sm px-4 py-8 rounded-md  flex justify-end gap-x-4'>
             {/* filter  */}
                <div className="filter_btn">
-                      <select name="" id="" className='w-52 text-lg capitalize border-[2px] focus:outline-[#FF0070] border-[#FF0070] p-2 rounded-sm font-semibold cursor-pointer' defaultValue='Filter By Catagory'>
+                      <select name="" id="" className='w-52 text-lg capitalize border-[2px] focus:outline-[#FF0070] border-[#FF0070] p-2 rounded-sm font-semibold cursor-pointer' defaultValue='Filter By Catagory' onChange={(e) => setFilter(e.target.value)}>
                            <option value="Filter By Catagory" disabled>Filter By Catagory</option>
-                           <option  value="All">All</option>
                            <option  value="Planned">Planned</option>
                            <option  value="In Progress"> In Progress</option>
                            <option  value="Completed"> Completed</option>
                       </select>
                </div>
                {/* sort button */}
-               <div className="sort_btn bg-[#FF0070] p-2  text-white font-semibold rounded-sm cursor-pointer">
-                    <button className='uppercase px-2'> Sort By Upvote</button>
+               <div className="sort_btn bg-[#FF0070] p-2  text-white font-semibold rounded-sm ">
+                    <button className='uppercase px-2 cursor-pointer' onClick={() => setSort('upvote')}> Sort By Upvote</button>
                </div>
 
                 {/* reset button */}
-               <div className="sort_btn bg-[#FF0070] p-2  text-white font-semibold rounded-sm cursor-pointer">
-                    <button className='uppercase px-2'> Reset</button>
+               <div className="sort_btn bg-[#FF0070] p-2  text-white font-semibold rounded-sm ">
+                    <button className='uppercase px-2 cursor-pointer' onClick={handleResetBtn}> Reset</button>
                </div>
 
 
@@ -44,7 +50,7 @@ const RoadMapItem = () => {
                 {
                     roadmapItem.map((item) => {
 
-                      return    <RoadMapCard  key={item?._id} road_map_item={item} isLoading={isLoading}></RoadMapCard>
+                      return    <div className=''><RoadMapCard  key={item?._id} road_map_item={item} isLoading={isLoading}></RoadMapCard></div>
                     })
                 }
           </div>
